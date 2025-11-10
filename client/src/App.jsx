45543e6b37f72eb6d36c88a30f53ccf5b221bfcc
Niloufar Heidarzade <Niiloufar.Heidarzadeh@gmail.com>
2025-11-10 19:11:31 +0330

    create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Feed from "./pages/Feed";
+import Messages from "./pages/Messages";
+import Connections from "./pages/Connections";
+import Discover from "./pages/Discover";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
-    <div></div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<Login />}>
+          <Route index element={<Feed />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="messages/:userId" element={<Chat />} />
+          <Route path="connections" element={<Connections />} />
+          <Route path="discover" element={<Discover />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="profile/:userId" element={<Profile />} />
+          <Route path="create-post" element={<CreatePost />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
